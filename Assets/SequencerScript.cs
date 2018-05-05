fe17c0d78e764fb40288bf99cd10dd7a935f0f78
Hoàng Anh Tú <tuha263@gmail.com>
2018-05-05 12:21:42 +0700

    Add audio helm

---
+++ Assets/SequencerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequencerScript : MonoBehaviour {
+
+	public AudioHelm.Sequencer sequencer;
+
+	public void PlayChord(int[] chord) {
+		sequencer.Clear ();
+		sequencer.length = GameControl.instance.CurrentSeqLength;
+		for (int i = 0; i < chord.Length; i++) {
+			int note = chord [i] + 12;
+			sequencer.AddNote (note, 0, GameControl.instance.CurrentSeqLength, 1);
+		}
+		if (!sequencer.isActiveAndEnabled) {
+			sequencer.enabled = true;
+			sequencer.StartOnNextCycle ();
+		}
+	}
+}
