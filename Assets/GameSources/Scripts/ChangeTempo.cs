80812edd40f77e9c6bad9ca333b921bc18251509
lukemorse <luke.morse@gmail.com>
2018-05-28 15:57:43 +0700

    add clock and tempo function for slider

---
+++ Assets/GameSources/Scripts/ChangeTempo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTempo : MonoBehaviour {
+
+	public int value;
+	public AudioHelm.AudioHelmClock clock;
+	Slider slider;
+
+	void Start() {
+		slider = GetComponentInParent<Slider>();
+	}
+	public void SetTempo() {
+		clock.bpm = slider.value;
+	}
+}
