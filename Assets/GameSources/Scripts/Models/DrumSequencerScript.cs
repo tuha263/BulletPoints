62098faca95b4fca537c25d248d84309da213ce5
lukemorse <luke.morse@gmail.com>
2018-06-27 12:38:02 +0700

    add drum sequencer script, alter prefab

---
+++ Assets/GameSources/Scripts/Models/DrumSequencerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioHelm;
+
+public class DrumSequencerScript : MonoBehaviour {
+
+	public Sampler sampler;
+	public UnityEngine.Audio.AudioMixerGroup mixerGroup;
+
+	// Use this for initialization
+	void Start () {
+		LoadDrumSounds();
+	}
+	
+	public void LoadDrumSounds() {
+		int minKey = 0;
+		Object[] sampleArray = Resources.LoadAll("DrumSamples/1");
+		for (int i = 0; i < sampleArray.Length; i++)
+		{
+			Keyzone keyZone = sampler.AddKeyzone();
+			keyZone.minKey = minKey;
+			keyZone.rootKey = minKey + 2;
+			keyZone.maxKey = minKey + 5;
+			keyZone.audioClip = sampleArray[i] as AudioClip;
+			keyZone.mixer = mixerGroup;
+			minKey += 5;			
+		}
+	}
+}
